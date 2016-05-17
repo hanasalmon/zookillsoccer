aa340fa5ba5c4c27b9d9feb41e187817d066b622
student <student@ai251lab0216m01.aicala.edmc.stu>
2016-05-17 11:15:26 -0700

    added additional

---
+++ modules/js/sample.js
@@ -0,0 +1,38 @@
+class GameObject {
+
+//initializer function
+constructor (input1, input2 ) {
+  this.message = input1;
+  this.number - input2;
+}
+//internal member function
+showMessage (){
+     return this.message;
+}
+
+//internal member function
+showNumber (){
+   return this.number;
+}
+
+}; //end of class
+
+var myObj = new GameObject("hail trump?" ,"666");
+class Character extends GameObject {
+  constructor (input1, input2,type)
+  super(input1, input2);  //initialize the'parent' GameObject
+  this.type = type;
+
+}
+ } //end of class
+
+ ///////////////////////////////
+ class Animal mextends Animal {
+     constructor (input1, input2,type,animalType)
+     super(input1, input2, type)
+ };
+
+ class Lion extends Animal {
+   constructor (input1, input2,type,animalType)
+   super(input1, input2, type)
+ };
